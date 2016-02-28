5ec55a8c03c16b51bda4274fcd69c92f607f66be
Kyle Into <kyleatyahoo@gmail.com>
2016-02-28 13:47:52 -0500

    teague is learning

--- js/createNotes.js
+++ js/createNotes.js
@@ -1,14 +1,9 @@
 var totalNotes = 0;
 function createNote(notePos){
-  var note = $("<i class='flaticon-music' alt='music-note' width='20px' height='40px' style='position:absolute; right:0px; top:" + notePos + "px;' id='note" + totalNotes + "'>");
+  var note = ("<i class='flaticon-music' alt='music-note' style='position:absolute; right:0px; top:40px;' id='note" + totalNotes + "'>");
   
   $("#sheetmusic").append(note);
-  setTimeout(function(){
-  console.log("hello");
-  //	$("#note"  + totalNotes).animate({left: "-500"}, 10000);
-  	  	$("#note" + totalNotes-1).css("border", "5px solid red");
-  }, 3000);
-
+  $("#note"  + totalNotes).animate({right: "+=2000"}, 15000);
   totalNotes++;
 }
 
