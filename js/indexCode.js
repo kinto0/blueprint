5864e66501ee175efc9a761f2fdd304ba7815a59
E. Xing <eric.tao.xing@gmail.com>
2016-02-28 16:26:24 -0500

    fixed clefs

--- js/indexCode.js
+++ js/indexCode.js
@@ -19,51 +19,11 @@ function createNote(x){
 	sound.pause();
 	sound.currentTime=0;
 	sound.play();
-	sound.click()
 }
 
 window.addEventListener("keydown", checkKeyPressed, false);
 
 function checkKeyPressed(event) {
-	if(event.keyCode == 67){
-		createNote("c");
-	}
-	if(event.keyCode == 70){
-		createNote("c1");
-	}
-	if(event.keyCode == 86){
-		createNote("d");
-	}
-	if(event.keyCode == 71){
-		createNote("d1");
-	}
-	if(event.keyCode == 66){
-		createNote("e");
-	}
-	if(event.keyCode == 78){
-		createNote("f");
-	}
-	if(event.keyCode == 74){
-		createNote("f1");
-	}
-	if(event.keyCode == 77){
-		createNote("g");
-	}
-	if(event.keyCode == 75){
-		createNote("g1");
-	}
-	if(event.keyCode == 188){
-		createNote("a");
-	}
-	if(event.keyCode == 76){
-		createNote("a1");
-	}
-	if(event.keyCode == 190){
-		createNote("b");
-	}
-}
-
-$("#g").click(function() {
 	if(event.keyCode == 67){
 		createNote("c");
 	}
