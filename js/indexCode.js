cafdb4f171d97d488182322181f87784dad51451
E. Xing <eric.tao.xing@gmail.com>
2016-02-28 16:15:50 -0500

    fixed keys

--- js/indexCode.js
+++ js/indexCode.js
@@ -4,7 +4,7 @@ var totalNotes = 0;
 
 
 function createNote(x){
-	
+
 	topOffset = 0;
 	//topOffset = $(("#clef" + x.toUpperCase()).position().top);
 	var note = ("<i class='flaticon-music' alt='music-note' style='transform:scale(2); position:absolute; right:0px; top:" + topOffset + ";' id='note" + totalNotes + "'>");
@@ -19,50 +19,85 @@ function createNote(x){
 	sound.pause();
 	sound.currentTime=0;
 	sound.play();
-
-
+	sound.click()
 }
 
 window.addEventListener("keydown", checkKeyPressed, false);
- 
+
 function checkKeyPressed(event) {
-if(event.keyCode == 67){
-	createNote("c");
-}
-if(event.keyCode == 70){
-	createNote("c1");
-}
-if(event.keyCode == 86){
-	createNote("d");
-}
-if(event.keyCode == 71){
-	createNote("d1");
-}
-if(event.keyCode == 66){
-	createNote("e");
-}
-if(event.keyCode == 78){
-	createNote("f");
-}
-if(event.keyCode == 74){
-	createNote("f1");
-}
-if(event.keyCode == 77){
-	createNote("g");
-}
-if(event.keyCode == 75){
-	createNote("g1");
-}
-if(event.keyCode == 188){
-	createNote("a");
-}
-if(event.keyCode == 76){
-	createNote("a1");
-}
-if(event.keyCode == 190){
-	createNote("b");
-}
+	if(event.keyCode == 67){
+		createNote("c");
+	}
+	if(event.keyCode == 70){
+		createNote("c1");
+	}
+	if(event.keyCode == 86){
+		createNote("d");
+	}
+	if(event.keyCode == 71){
+		createNote("d1");
+	}
+	if(event.keyCode == 66){
+		createNote("e");
+	}
+	if(event.keyCode == 78){
+		createNote("f");
+	}
+	if(event.keyCode == 74){
+		createNote("f1");
+	}
+	if(event.keyCode == 77){
+		createNote("g");
+	}
+	if(event.keyCode == 75){
+		createNote("g1");
+	}
+	if(event.keyCode == 188){
+		createNote("a");
+	}
+	if(event.keyCode == 76){
+		createNote("a1");
+	}
+	if(event.keyCode == 190){
+		createNote("b");
+	}
 }
 
-
-
+$("#g").click(function() {
+	if(event.keyCode == 67){
+		createNote("c");
+	}
+	if(event.keyCode == 70){
+		createNote("c1");
+	}
+	if(event.keyCode == 86){
+		createNote("d");
+	}
+	if(event.keyCode == 71){
+		createNote("d1");
+	}
+	if(event.keyCode == 66){
+		createNote("e");
+	}
+	if(event.keyCode == 78){
+		createNote("f");
+	}
+	if(event.keyCode == 74){
+		createNote("f1");
+	}
+	if(event.keyCode == 77){
+		createNote("g");
+	}
+	if(event.keyCode == 75){
+		createNote("g1");
+	}
+	if(event.keyCode == 188){
+		createNote("a");
+	}
+	if(event.keyCode == 76){
+		createNote("a1");
+	}
+	if(event.keyCode == 190){
+		createNote("b");
+	}
+}
